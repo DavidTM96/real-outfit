7cc6bd5a1ccac0b0ee9a7a1115907d21a0f38784
David Torrez <dtorrez1996@gmail.com>
2025-04-04 00:33:31 -0400

    Delete prisma const since now imported from prisma.ts

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,16 +1,14 @@
-'use server';
-import { convertToPlainObject } from '@/lib/utils';
-import { PrismaClient } from '@prisma/client';
-import { LATEST_PRODUCTS_LIMIT } from '../constants';
+"use server";
+import { prisma } from "@/db/prisma";
+import { convertToPlainObject } from "@/lib/utils";
+import { LATEST_PRODUCTS_LIMIT } from "../constants";
 
 // Get latest products
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: {
-      createdAt: 'desc',
+      createdAt: "desc",
     },
   });
 
