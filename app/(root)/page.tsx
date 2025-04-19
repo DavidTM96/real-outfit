30938ccef3d04f76e1246d7002bfd345a879d1b9
David Torrez <dtorrez1996@gmail.com>
2025-04-19 11:01:57 -0400

    Cast latestProducts as unknown and Product array

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,16 @@
-import ProductList from '@/components/shared/product/product-list';
-import { getLatestProducts } from '@/lib/actions/product.actions';
+import ProductList from "@/components/shared/product/product-list";
+import { getLatestProducts } from "@/lib/actions/product.actions";
+import { Product } from "@/types";
 
 const HomePage = async () => {
   const latestProducts = await getLatestProducts();
 
   return (
     <>
-      <ProductList data={latestProducts} title='Newest Arrivals' />
+      <ProductList
+        data={latestProducts as unknown as Product[]}
+        title="Newest Arrivals"
+      />
     </>
   );
 };
