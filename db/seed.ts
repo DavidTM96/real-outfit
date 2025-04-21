769e0d3034f8bcaa0e030019afdba2446fb60fe2
David Torrez <dtorrez1996@gmail.com>
2025-04-21 18:55:31 -0400

    Install Next Auth

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 import sampleData from "./sample-data";
 
+// We run 'tsx ./db/seed' to seed the database, then we can reload Prisma Studio to see the data in the database.
+
 async function main() {
   const prisma = new PrismaClient();
   await prisma.product.deleteMany();
