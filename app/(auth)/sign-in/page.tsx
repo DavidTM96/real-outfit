ac0da4246e2d709e608e6b2d0cb6352aa6fa6153
David Torrez <dtorrez1996@gmail.com>
2025-04-21 20:18:02 -0400

    Create CredentialsSignInForm

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -9,6 +9,7 @@ import { APP_NAME } from "@/lib/constants";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import CredentialsSignInForm from "./credentials-signin-form";
 
 export const metadata: Metadata = {
   title: "Sign In",
@@ -34,7 +35,7 @@ const SignInPage = () => {
           </CardDescription>
         </CardHeader>
         <CardContent className="space-y-4">
-          {/* SignInForm component will be placed here */}
+          <CredentialsSignInForm />
         </CardContent>
       </Card>
     </div>
