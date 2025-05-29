2b93e34de8968959101d2c4aa79d4afe2f680923
David Torrez <dtorrez1996@gmail.com>
2025-05-28 21:43:43 -0400

    Create form for shipping address

---
+++ app/(root)/shipping-address/shipping-address-form.tsx
@@ -0,0 +1,189 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { updateUserAddress } from "@/lib/actions/user.actions";
+import { shippingAddressDefaultValues } from "@/lib/constants";
+import { shippingAddresssSchema } from "@/lib/validators";
+import { ShippingAddress } from "@/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ArrowRight, Loader } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { ControllerRenderProps, SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
+
+const ShippingAddressForm = ({ address }: { address: ShippingAddress }) => {
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof shippingAddresssSchema>>({
+    resolver: zodResolver(shippingAddresssSchema),
+    defaultValues: address || shippingAddressDefaultValues,
+  });
+
+  const [isPending, startTransition] = useTransition();
+
+  const onSubmit: SubmitHandler<
+    z.infer<typeof shippingAddresssSchema>
+  > = async (values) => {
+    startTransition(async () => {
+      const res = await updateUserAddress(values);
+
+      if (!res.success) {
+        toast.error(res.message);
+        return;
+      }
+      router.push("/payment-method");
+    });
+  };
+
+  return (
+    <>
+      <div className="max-w-md mx-auto space-y-4">
+        <h1 className="h2-bold mt-4">Shipping Address</h1>
+        <p className="text-sm text-muted-foreground">
+          Please enter an address to ship to
+        </p>
+        <Form {...form}>
+          <form
+            method="post"
+            className="space-y-4"
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <div className="flex flex-col md:flex-row gap-5">
+              <FormField
+                control={form.control}
+                name="fullName"
+                render={({
+                  field,
+                }: {
+                  field: ControllerRenderProps<
+                    z.infer<typeof shippingAddresssSchema>,
+                    "fullName"
+                  >;
+                }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>Full Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter full name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex flex-col md:flex-row gap-5">
+              <FormField
+                control={form.control}
+                name="streetAddress"
+                render={({
+                  field,
+                }: {
+                  field: ControllerRenderProps<
+                    z.infer<typeof shippingAddresssSchema>,
+                    "streetAddress"
+                  >;
+                }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>Address</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter address" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex flex-col md:flex-row gap-5">
+              <FormField
+                control={form.control}
+                name="city"
+                render={({
+                  field,
+                }: {
+                  field: ControllerRenderProps<
+                    z.infer<typeof shippingAddresssSchema>,
+                    "city"
+                  >;
+                }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>City</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter city" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex flex-col md:flex-row gap-5">
+              <FormField
+                control={form.control}
+                name="postalCode"
+                render={({
+                  field,
+                }: {
+                  field: ControllerRenderProps<
+                    z.infer<typeof shippingAddresssSchema>,
+                    "postalCode"
+                  >;
+                }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>Postal code</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter postal code" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex flex-col md:flex-row gap-5">
+              <FormField
+                control={form.control}
+                name="country"
+                render={({
+                  field,
+                }: {
+                  field: ControllerRenderProps<
+                    z.infer<typeof shippingAddresssSchema>,
+                    "country"
+                  >;
+                }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>Country</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter country" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="flex gap-2">
+              <Button type="submit" disabled={isPending}>
+                {isPending ? (
+                  <Loader className="size-4 animate-spin" />
+                ) : (
+                  <ArrowRight className="size-4" />
+                )}
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default ShippingAddressForm;
