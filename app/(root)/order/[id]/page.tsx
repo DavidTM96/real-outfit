1c447453a07effc158c5e39198d054daf085a459
David Torrez <dtorrez1996@gmail.com>
2025-07-15 15:53:45 -0400

    Pass isAdmin prop to OrdersDetailsTable component

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { auth } from "@/auth";
 import { getOrderById } from "@/lib/actions/order.actions";
 import { ShippingAddress } from "@/types";
 import { Metadata } from "next";
@@ -19,6 +20,8 @@ const OrderDetailsPage = async (props: {
 
   if (!order) notFound();
 
+  const session = await auth();
+
   return (
     <OrderDetailsTable
       order={{
@@ -26,6 +29,7 @@ const OrderDetailsPage = async (props: {
         shippingAddress: order.shippingAddress as ShippingAddress,
       }}
       paypalClientId={process.env.PAYPAL_CLIENT_ID || "sb"}
+      isAdmin={session?.user?.role === "admin" || false}
     />
   );
 };
