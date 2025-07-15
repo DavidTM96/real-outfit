f9d4e08ad7006f9083f3058c4d7c3edebb825b2a
David Torrez <dtorrez1996@gmail.com>
2025-07-15 15:27:13 -0400

    Create an action to update order to paid COD

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -411,3 +411,24 @@ export async function deleteOrder(id: string) {
     };
   }
 }
+
+// Update COD order to paid
+export async function updateOrderToPaidCOD(orderId: string) {
+  try {
+    await updateOrderToPaid({
+      orderId,
+    });
+
+    revalidatePath(`/order/${orderId}`);
+
+    return {
+      success: true,
+      message: "Order marked as paid",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: formatError(error),
+    };
+  }
+}
