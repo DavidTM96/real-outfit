70d35a1a0456ad65162fbd6f903c48579d140850
David Torrez <dtorrez1996@gmail.com>
2025-07-22 16:18:00 -0400

    Create an action to update a user

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -14,6 +14,7 @@ import {
   shippingAddresssSchema,
   signInFormSchema,
   signUpFormSchema,
+  updateUserSchema,
 } from "../validators";
 
 // Sign in the user with credentials
@@ -221,6 +222,31 @@ export async function deleteUser(id: string) {
     return {
       success: true,
       message: "User deleted successfully",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: formatError(error),
+    };
+  }
+}
+
+// Update a user
+export async function updateUser(user: z.infer<typeof updateUserSchema>) {
+  try {
+    await prisma.user.update({
+      where: { id: user.id },
+      data: {
+        name: user.name,
+        role: user.role,
+      },
+    });
+
+    revalidatePath("/admin/users");
+
+    return {
+      success: true,
+      message: "User updated successfully",
     };
   } catch (error) {
     return {
