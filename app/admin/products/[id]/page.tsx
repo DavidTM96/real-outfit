472406249a6ccfe5739a4b8bb80c43b0637a9473
David Torrez <dtorrez1996@gmail.com>
2025-07-22 12:03:23 -0400

    Create page for admin to update products

---
+++ app/admin/products/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Update Product",
+};
+
+const AdminProductUpdatePage = () => {
+  return <>Update product page</>;
+};
+
+export default AdminProductUpdatePage;
