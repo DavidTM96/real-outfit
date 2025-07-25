ee1d55246a741a7606e1065c426152fdda5bee4c
David Torrez <dtorrez1996@gmail.com>
2025-07-25 15:54:30 -0400

    Implement CategoryDrawer component in Header

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,22 +1,24 @@
-import { APP_NAME } from '@/lib/constants';
-import Image from 'next/image';
-import Link from 'next/link';
-import Menu from './menu';
+import { APP_NAME } from "@/lib/constants";
+import Image from "next/image";
+import Link from "next/link";
+import CategoryDrawer from "./category-drawer";
+import Menu from "./menu";
 
 const Header = () => {
   return (
-    <header className='w-full border-b'>
-      <div className='wrapper flex-between'>
-        <div className='flex-start'>
-          <Link href='/' className='flex-start ml-4'>
+    <header className="w-full border-b">
+      <div className="wrapper flex-between">
+        <div className="flex-start ml-4">
+          <CategoryDrawer />
+          <Link href="/" className="flex-start ml-4">
             <Image
-              src='/images/logo.svg'
+              src="/images/logo.svg"
               alt={`${APP_NAME} logo`}
               height={48}
               width={48}
               priority={true}
             />
-            <span className='hidden lg:block font-bold text-2xl ml-3'>
+            <span className="hidden lg:block font-bold text-2xl ml-3">
               {APP_NAME}
             </span>
           </Link>
