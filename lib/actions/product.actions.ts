13146ee411a9b59a72a324bd87b83f2c1c0af4e4
David Torrez <dtorrez1996@gmail.com>
2025-07-29 16:15:27 -0400

    Create queryFilter variable in getAllProducts action

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,6 +1,7 @@
 "use server";
 import { prisma } from "@/db/prisma";
 import { convertToPlainObject, formatError } from "@/lib/utils";
+import { Prisma } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
 import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from "../constants";
@@ -55,6 +56,17 @@ export async function getAllProducts({
   rating?: string;
   sort?: string;
 }) {
+  // Query filter
+  const queryFilter: Prisma.ProductWhereInput =
+    query && query !== "all"
+      ? {
+          name: {
+            contains: query,
+            mode: "insensitive",
+          } as Prisma.StringFilter,
+        }
+      : {};
+
   const data = await prisma.product.findMany({
     orderBy: {
       createdAt: "desc",
