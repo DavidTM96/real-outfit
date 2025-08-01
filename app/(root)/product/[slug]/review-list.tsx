fe7d692a9651cf10ed4a596895d21780fc391502
David Torrez <dtorrez1996@gmail.com>
2025-08-01 16:27:23 -0400

    Render text for no reviews scenario, and sign-in link for writing reviews

--- app/(root)/product/[slug]/review-list.tsx
+++ app/(root)/product/[slug]/review-list.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { Review } from "@/types";
+import Link from "next/link";
+import { useState } from "react";
+
 const ReviewList = ({
   userId,
   productId,
@@ -9,9 +13,28 @@ const ReviewList = ({
   productId: string;
   productSlug: string;
 }) => {
-  console.log(userId, productId, productSlug);
+  const [reviews, setReviews] = useState<Review[]>([]);
 
-  return <>Review List</>;
+  return (
+    <div className="space-y-4">
+      {reviews.length === 0 && <div>No reviews yet</div>}
+      {userId ? (
+        <>{/* Review Form Here */}</>
+      ) : (
+        <div>
+          Please
+          <Link
+            className="text-blue-700 px-2"
+            href={`/sign-in?callbackUrl=/product/${productSlug}`}
+          >
+            sign in
+          </Link>
+          to write a review
+        </div>
+      )}
+      <div className="flex-flex-col gap-3">{/* REVIEWS HERE */}</div>
+    </div>
+  );
 };
 
 export default ReviewList;
