bfde7c379d1e000457fb8c68579e727ddc21f002
David Torrez <dtorrez1996@gmail.com>
2025-08-01 16:18:38 -0400

    Create ReviewList component

---
+++ app/(root)/product/[slug]/review-list.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+const ReviewList = ({
+  userId,
+  productId,
+  productSlug,
+}: {
+  userId: string;
+  productId: string;
+  productSlug: string;
+}) => {
+  console.log(userId, productId, productSlug);
+
+  return <>Review List</>;
+};
+
+export default ReviewList;
