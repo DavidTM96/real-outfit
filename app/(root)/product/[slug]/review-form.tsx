e9557c4fcb889c042d387eeaa35ee2e30337f75d
David Torrez <dtorrez1996@gmail.com>
2025-08-06 13:08:58 -0400

    Create ReviewForm component

---
+++ app/(root)/product/[slug]/review-form.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+const ReviewForm = ({
+  userId,
+  productId,
+  onReviewSubmitted,
+}: {
+  userId: string;
+  productId: string;
+  onReviewSubmitted?: () => void;
+}) => {
+  return <>Review Form</>;
+};
+
+export default ReviewForm;
